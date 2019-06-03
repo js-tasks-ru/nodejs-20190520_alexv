90e222906731a3b2b2f45715240db6ba43b7ecb7
alexvlife <alexvlife@gmail.com>
2019-06-04 01:13:41 +0300

    add solution for 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -1,15 +1,48 @@
 const stream = require('stream');
 const os = require('os');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.encoding = options.encoding;
+    this.waitingChunk = '';
   }
 
   _transform(chunk, encoding, callback) {
+    if (!this.myStringDecoder) {
+      this.myStringDecoder = new StringDecoder(this.encoding);
+    }
+
+    const encodedText = this.myStringDecoder.write(chunk);
+    const encodedChunks = encodedText.split(os.EOL);
+    const waitingChunk = encodedChunks.pop(); // 'текст' или пустая строка ''
+
+    encodedChunks.forEach((encodedChunk, index) => {
+      let chunkForPush = encodedChunk;
+
+      if (this.waitingChunk && index === 0) {
+        chunkForPush = this.waitingChunk + encodedChunk;
+        this._resetWaitingChunk();
+      }
+
+      this.push(chunkForPush);
+    });
+
+    this._resizeWaitingChunk(waitingChunk);
+    callback();
   }
 
   _flush(callback) {
+    this.waitingChunk && callback(null, this.waitingChunk);
+  }
+
+  _resizeWaitingChunk(chunk) {
+    this.waitingChunk += chunk;
+  }
+
+  _resetWaitingChunk() {
+    this.waitingChunk = '';
   }
 }
 
