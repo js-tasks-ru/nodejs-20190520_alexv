f78f3185bca70bae2ab660dca732ba71480831c6
alexvlife <alexvlife@gmail.com>
2019-06-14 02:02:57 +0300

    add solution for 4-module 2-task

--- 4-module/1-task/utils.js
+++ 4-module/1-task/utils.js
@@ -1,14 +1,13 @@
-const SLASH_REGEXP = /\//g;
+const SLASH_CHAR = '/';
 
 class Utils {
-  // Функция определяет присутствуют ли вложенные папки в указанном пути к файлу
+  // Функция определяет присутствуют ли вложенные папки в указанном пути к файлу.
   static isNestedPath(path) {
     if (typeof(path) !== 'string') {
       return;
     }
 
-    const nestings = path.match(SLASH_REGEXP); // массив со слешами "/", либо null
-    return Boolean(nestings);
+    return path.includes(SLASH_CHAR);
   }
 }
 
--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,23 +1,73 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
+const LimitExceededError = require('./LimitExceededError');
+const Utils = require('../1-task/utils');
 
 const server = new http.Server();
+const KILO = 1024;
+const UPLOAD_FILE_MAX_SIZE_MB = 1;
+const bytesLimit = UPLOAD_FILE_MAX_SIZE_MB * KILO ** 2;
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
+
+  if (Utils.isNestedPath(pathname)) {
+    res.statusCode = 400;
+    res.end('Bad Request');
+    return;
+  }
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'POST':
+      const limitStream = new LimitSizeStream({limit: bytesLimit});
+      const writeStream = fs.createWriteStream(filepath, {flags: 'wx'});
 
+      req.pipe(limitStream).pipe(writeStream);
+
+      writeStream
+          .on('error', (err) => {
+            if (err.code === 'EEXIST') {
+              res.statusCode = 409;
+              res.end('already exists');
+            } else {
+              res.statusCode = 500;
+              res.end('internal error');
+            }
+          })
+          .on('close', () => {
+            res.statusCode = 201;
+            res.end('ok');
+          });
+
+      limitStream
+          .on('error', (err) => {
+            if (err instanceof LimitExceededError) {
+              res.statusCode = 413;
+              res.end('Payload Too Large');
+              fs.unlink(filepath, () => {});
+              limitStream.destroy();
+            } else {
+              res.statusCode = 500;
+              res.end('internal error');
+            }
+          });
       break;
 
     default:
       res.statusCode = 501;
       res.end('Not implemented');
   }
+
+  req.on('close', () => {
+    if (!res.finished) {
+      fs.unlink(filepath, () => {});
+    }
+  });
 });
 
 module.exports = server;
